a50bb7b0d71ab8bcfd76a4d0fb07225b564a954a
Pedro Ruiz <pedroruizf@gmail.com>
2019-03-02 20:31:14 +0100

    cambios en ejemplos de manual

--- manual/ejemplos/ejemplo_03b/ejemplo_03b.ino
+++ manual/ejemplos/ejemplo_03b/ejemplo_03b.ino
@@ -13,25 +13,25 @@ void loop() {
 
   case forward://si pulsamos el botón delantero, se enciende led delantero, se mueve 7 cm hacia delante, y se apaga el led delantero
     mirobot.ledON (forward);
-    mirobot.drive (7, 10);
+    mirobot.driveD (7, 10);
     mirobot.ledOFF (forward);
     break;
 
   case backward://si pulsamos el botón trasero, se enciende led trasero, se mueve 7 cm hacia atrás, y se apaga el led trasero
     mirobot.ledON (backward);
-    mirobot.drive (-7, 10);
+    mirobot.driveD (-7, 10);
     mirobot.ledOFF (backward);
     break;
 
   case right://si pulsamos el botón derecho, se enciende led derecho, gira 45º hacia la derecha, y se apaga el led derecho
     mirobot.ledON (right);
-    mirobot.turn (45, 10);
+    mirobot.turnA (45, 10);
     mirobot.ledOFF (right);
     break;
 
   case left://si pulsamos el botón izquierdo, se enciende led izquierdo, gira 45º hacia la izquierda, y se apaga el led izquierdo
     mirobot.ledON (left);
-    mirobot.turn (-45, 10);
+    mirobot.turnA (-45, 10);
     mirobot.ledOFF (left);
     break;
 
